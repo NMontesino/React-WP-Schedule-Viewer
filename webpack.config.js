d2379dda2330012ecc14873521ef91ee09a0eec7
Nick Montesino <namontesino@gmail.com>
2021-02-22 18:28:27 -0500

    Set Up Initial React Project Structure

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path")
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = 
+{
+    entry: "./src/index.js",
+    output: 
+    {
+        path: path.join(__dirname, "/dist"),
+        filename: "index_bundle.js"
+    },
+    devtool: 'source-map',
+    devServer:
+    {
+        contentBase: 'public'
+    },
+    module: 
+    {
+        rules: 
+        [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 
+                {
+                    loader: "babel-loader",
+                }
+            }
+        ]
+    },
+    plugins: 
+    [
+        new HTMLWebpackPlugin(
+        {
+            template: './src/index.html'
+        })
+    ]
+}
